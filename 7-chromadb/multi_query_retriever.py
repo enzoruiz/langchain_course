45b0b022f3ba9b2a292f0098037aebeb5c0f2c63
Enzo Ruiz <enzo.ruizpelaez@crehana.com>
2026-01-30 19:30:05 -0500

    minor: basic rag assistant example

---
+++ 7-chromadb/multi_query_retriever.py
@@ -0,0 +1,26 @@
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain_classic.retrievers.multi_query import MultiQueryRetriever
+
+
+vectorstore = Chroma(
+    embedding_function=OpenAIEmbeddings(model="text-embedding-3-large"),
+    persist_directory="/home/enzo/projects/langchain_course/7-chromadb/chroma_db"
+)
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+base_retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 2})
+retriever = MultiQueryRetriever.from_llm(retriever=base_retriever, llm=llm)
+
+query = "¿Dónde se encuentra el local del contrato en el que participa María Jiménez Campos?"
+print(f"Consulta: {query}")
+
+results = retriever.invoke(query)
+
+print(f"Top documentos mas similares a la consulta:")
+for i, doc in enumerate(results):
+    print(f"Contenido: {doc.page_content}")
+    print(f"Metadatos: {doc.metadata}")
+    print("====================================================================")
---
+++ 7-chromadb/retrievers_langchain.py
@@ -0,0 +1,21 @@
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+
+
+vectorstore = Chroma(
+    embedding_function=OpenAIEmbeddings(model="text-embedding-3-large"),
+    persist_directory="/home/enzo/projects/langchain_course/7-chromadb/chroma_db"
+)
+
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 2})
+
+query = "Donde se encuentra el local del contrato en el que participa María Jiménez Campos?"
+print(f"Consulta: {query}")
+
+results = retriever.invoke(query)
+
+print(f"Top 2 documentos mas similares a la consulta:")
+for i, doc in enumerate(results):
+    print(f"Contenido: {doc.page_content}")
+    print(f"Metadatos: {doc.metadata}")
+    print("====================================================================")
